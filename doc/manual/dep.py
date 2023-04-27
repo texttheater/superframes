0ad1cf1e57278304380482934853fcad042ca03f
Kilian Evang <noreply@texttheater.net>
2023-04-27 19:03:35 +0200

    Manual (WIP)

---
+++ doc/manual/dep.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+
+import sys
+
+
+BS = '\\'
+TOKSEP = ' \\& '
+
+
+def clean(token):
+    if token.startswith('*') and token.endswith('*'):
+        return token[1:-1]
+    return token.rsplit('_', 1)[0]
+
+
+if __name__ == '__main__':
+    print(sys.argv, file=sys.stderr)
+    tokens = sys.argv[1:]
+    pred_idx = -1
+    for i, token in enumerate(tokens):
+        if token.startswith('*') and token.endswith('*'):
+            pred_idx = i
+    if pred_idx == 1:
+        print(f'ERROR: predicate not marked: {" ".join(tokens)}',
+                file=sys.stderr)
+        sys.exit(1)
+    print(f'''{BS}begin{{dependency}}
+    {BS}begin{{deptext}}
+        {TOKSEP.join(clean(t) for t in tokens)} {BS}{BS}
+    {BS}end{{deptext}}''')
+    height = 1
+    for i in reversed(range(0, pred_idx)):
+        if '_' in tokens[i]:
+            rel = tokens[i].rsplit('_', 1)[1]
+            print(f'    {BS}depedge[edge height={height}{BS}baselineskip]{{{pred_idx + 1}}}{{{i + 1}}}{{{rel}}}')
+            height += 1
+    height = 1
+    for i in range(pred_idx + 1, len(tokens)):
+        if '_' in tokens[i]:
+            rel = tokens[i].rsplit('_', 1)[1]
+            print(f'    {BS}depedge[edge height={height}{BS}baselineskip]{{{pred_idx + 1}}}{{{i + 1}}}{{{rel}}}')
+            height += 1
+    print('\\end{dependency}')
