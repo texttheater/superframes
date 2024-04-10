deb05fc533b134da8d422ccef9b41d14cc812496
Kilian Evang <noreply@texttheater.net>
2024-04-10 18:58:31 +0200

    Frames are now parsed

--- src/python/cusf.py
+++ src/python/cusf.py
@@ -11,19 +11,54 @@ import pyconll
 import blocks
 
 
-FRAME_LINE = re.compile(r'\[(?P<label>[^]]*)] (?P<text>.*?) \((?P<tokid>\d+)\)$')
+FRAME_LINE = re.compile(r'\[(?P<label>[^]]*)] (?P<text>.*?) \((?P<head>\d+)\)$')
+
+
+class Arg:
+
+    def __init__(self, head: int, text: str = '', label: str=''):
+        self.head = head
+        self.text = text
+        self.label = label
+
+    def to_line(self) -> str:
+        return f'[{self.label}] {self.text} ({self.head})'
+
+    @staticmethod
+    def from_line(line: str) -> 'Arg':
+        m = FRAME_LINE.match(line)
+        head = int(m.group('head'))
+        text = m.group('text')
+        label = m.group('label')
+        arg = Arg(head, text, label)
+        return arg
 
 
 class Frame:
 
-    def __init__(self, head: int, label: str='', args: Optional[dict]=None):
+    def __init__(self, head: int, text: str = '', label: str='', args: Optional[dict]=None):
         self.head = head
+        self.text = text
         self.label = label
         self.args = [] if args is None else args
 
-    @classmethod
+    def to_block(self) -> blocks.Block:
+        block = []
+        block.append(f'[{self.label}] {self.text} ({self.head})')
+        for arg in self.args:
+            block.append(arg.to_line())
+        return block
+
+    @staticmethod
     def from_block(block: blocks.Block) -> 'Frame':
-        raise NotImplemented()
+        m = FRAME_LINE.match(block[0])
+        head = int(m.group('head'))
+        text = m.group('text')
+        label = m.group('label')
+        frame = Frame(head, text, label)
+        for line in block[1:]:
+            frame.args.append(Arg.from_line(line))
+        return frame
 
 
 """Can't parse a frame? Represent it as a block."""
@@ -40,7 +75,7 @@ class Sentence:
         if self.frames is None:
             self.frames = []
         try:
-            frame = Frame.from_block(block, self.syntax)
+            frame = Frame.from_block(block)
             self.frames.append(frame)
         except:
             self.frames.append(block)
@@ -48,7 +83,10 @@ class Sentence:
     def write(self, io: TextIO=sys.stdout):
         print(self.syntax.conll(), file=io, end='')
         for frame in self.frames:
-            blocks.write(frame, io=io)
+            if isinstance(frame, Frame):
+                blocks.write(frame.to_block(), io=io)
+            else:
+                blocks.write(frame, io=io)
 
 
 def read(io: TextIO=sys.stdin) -> Iterable[Sentence]:
