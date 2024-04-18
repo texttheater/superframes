404353a2cfbcceba74357360ce0f9371c2740328
Kilian Evang <noreply@texttheater.net>
2024-04-18 13:50:55 +0200

    Switched to two-pass check
    * first add missing frames
    * then read file again, run checks
    * will make it easier to emit line number info with checks

--- src/python/check.py
+++ src/python/check.py
@@ -15,19 +15,30 @@ import cusf
 
 
 if __name__ == '__main__':
+    # Process command line
     logging.basicConfig(level=logging.INFO)
     arg_parser = argparse.ArgumentParser(description=__doc__)
     arg_parser.add_argument('file')
     args = arg_parser.parse_args()
+    # Make backup file
     backup_file = args.file + '~'
     shutil.copyfile(args.file, backup_file)
+    # Read file
     with open(args.file) as f:
         sentences = [s for s in cusf.read(f)]
+    # Add missing frames
+    with open(args.file, 'w') as f:
+        for sentence in sentences:
+            sentence.fill()
+            sentence.write(f)
+    # Read file again
+    with open(args.file) as f:
+        sentences = [s for s in cusf.read(f)]
+    # Run checks and emit warnings
     predicate_count = 0
     annotated_count = 0
     with open(args.file, 'w') as f:
         for sentence in sentences:
-            sentence.fill()
             for frame in sentence.frames:
                 if isinstance(frame, cusf.Frame):
                     predicate_count += 1
