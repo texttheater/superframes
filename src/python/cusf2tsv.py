2d01953eb87393593edce81f588007d063528f29
Kilian Evang <noreply@texttheater.net>
2026-03-03 14:28:23 +0100

    Changed export format
    Now is the same as for RRGparbank.

--- src/python/cusf2tsv.py
+++ src/python/cusf2tsv.py
@@ -8,7 +8,11 @@ ID, dependent token number, head token number, edge label, head label,
 annotator.
 """
 
+
 import argparse
+import json
+import logging
+import pathlib
 
 
 import cusf
@@ -16,25 +20,45 @@ import cusf
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description=__doc__)
-    arg_parser.add_argument('cusf', type=argparse.FileType())
+    arg_parser.add_argument('cusf', nargs='*', type=argparse.FileType())
     args = arg_parser.parse_args()
-    seen = set()
-    for sentence in cusf.read(args.cusf):
-        sentence_id = sentence.syntax[0].id
-        for frame in sentence.frames:
-            if not isinstance(frame, cusf.Frame):
+    data = {}
+    for f in args.cusf:
+        path = pathlib.Path(f.name)
+        annotator = path.stem
+        for sentence in cusf.read(f):
+            sentence_id = sentence.syntax[0].id
+            if sentence_id not in data:
+                data[sentence_id] = {}
+            if annotator in data[sentence_id]:
+                logging.warn(
+                    'duplicate sentence %s for annotator %s',
+                    sentence_id,
+                    annotator,
+                )
                 continue
-            head_id = frame.head
-            if (sentence_id, head_id) in seen:
-                continue # skip duplicate frame annotations
-            if not frame.is_completely_annotated():
-                continue
-            ok, _ = frame.check(sentence, 0, True, True, True)
-            if not ok:
-                continue
-            seen.add((sentence_id, head_id))
-            head_label = frame.label
-            for arg in frame.args:
-                arg_id = arg.head
-                arg_label = arg.label
-                print(sentence_id, head_id, head_label, arg_id, arg_label, sep='\t')
+            sa_data = {'roles': {}, 'status': 1, 'comment': None}
+            data[sentence_id][annotator] = sa_data
+            for frame in sentence.frames:
+                if not isinstance(frame, cusf.Frame):
+                    continue
+                head_id = frame.head
+                if head_id in sa_data['roles']:
+                    logging.warn(
+                        'duplicate frame %s in sentence %s for annotator %s',
+                        sentence_id,
+                        head_id,
+                        annotator,
+                    )
+                    continue
+                if not frame.is_completely_annotated():
+                    continue
+                ok, _ = frame.check(sentence, 0, True, True, True)
+                if not ok:
+                    continue
+                args = {}
+                sa_data['roles'][head_id] = {'args': args, 'frame': frame.label}
+                for arg in frame.args:
+                    args[arg.head] = arg.label
+    for sentence_id, sentence_data in data.items():
+        print(sentence_id, json.dumps(sentence_data), sep='\t')
